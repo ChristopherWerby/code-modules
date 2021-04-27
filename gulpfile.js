43380c7c9c42b145601fb4535730f06dda7e4815
Christopher Werby <cwerby@pipsqueak.com>
2021-04-27 13:01:24 -0700

    Change from CSS to Less. Move Less to app/. Add Gulp Task to convert Less to css/main.css. Comment out some bootstrap css files to make this work with less.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ const browsersync = require("browser-sync").create();
 const del = require("del");
 const gulp = require("gulp");
 const merge = require("merge-stream");
+const gulpless = require('gulp-less');
+const gulpautoprefixer = require('gulp-autoprefixer');
+
+//Set styles source file and destination folder
+const sourceStyles = './app/styles/main.less';
+const sourceStylesWatch = './app/styles/**/*';
+const destStylesFolder = './public/css';
+
 
 // BrowserSync
 function browserSync(done) {
@@ -42,20 +50,41 @@ function modules() {
   return merge(bootstrap, jquery);
 }
 
+// Gulp Task for Styles - Less and Autoprefixer
+function convertLess() {
+	const autoPrefixOpts = {
+		browsers: [
+			'last 2 versions',
+			'>5%',
+		]
+	}
+	return gulp
+		.src(sourceStyles)
+		.pipe(gulpless())
+		.pipe(gulpautoprefixer(autoPrefixOpts))
+		.pipe(gulp.dest(destStylesFolder));
+};
+
 // Watch files
 function watchFiles() {
   gulp.watch("./public/**/*.css", browserSyncReload);
   gulp.watch("./public/**/*.html", browserSyncReload);
 }
 
+function watchStyles() {
+	gulp.watch(sourceStylesWatch, styles);
+}
+
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
 const build = gulp.series(vendor);
-const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+const watch = gulp.series(build, gulp.parallel(watchFiles, watchStyles, browserSync));
+const styles = gulp.series(convertLess);
 
 // Export tasks
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
+exports.styles = styles;
 exports.default = build;
